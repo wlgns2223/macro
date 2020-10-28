d35c0cf1be64995109d6ae73a305f0af71faa295
jihoon <wlgns2223@gmail.com>
2020-10-28 21:50:26 +0100

    refactor: Implemented '__set_driver_options' feat: Implemented 'open_naver'

--- validator.py
+++ validator.py
@@ -1,6 +1,7 @@
 from bs4 import BeautifulSoup
 from selenium import webdriver
 import requests as req
+from fake_useragent import UserAgent
 
 
 class Validator:
@@ -12,15 +13,34 @@ class Validator:
         self.password = password
         self.chromedriver = None
 
-        self.init_selenium()
+        self.__init_selenium()
 
-    def init_selenium(self):
-        driver_path = './chromedriver'
+    def __set_driver_obtions(self):
+
+        ua = UserAgent()
+        ua = ua.random
+
         driver_option = webdriver.ChromeOptions()
-        driver_option.add_argument('headless')
 
+        # driver_option.add_argument('headless')
+        driver_option.add_argument(f'user-agent={ua}')
+
+        return driver_option
+
+    def __init_selenium(self):
+        driver_path = './chromedriver'
+        driver_option = self.__set_driver_obtions()
         self.chromedriver = webdriver.Chrome(
             executable_path=driver_path, chrome_options=driver_option)
 
-    def __open_naver(self):
+    def open_naver(self):
         self.chromedriver.get(self.__class__.URL)
+
+
+if __name__ == '__main__':
+
+    from validator import Validator
+
+    v = Validator(id='hello', password='world')
+    v.init_selenium()
+    v.open_naver()
